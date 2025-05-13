b65d004a5dbcc535f54b59f9c464cebf32253ebf
t2024-m0188 <t2024-m0188@t2024-m0188ui-MacBookAir.local>
2025-05-13 10:22:10 +0900

    test: movie_data_source_test

--- test/movie_data_source_test.dart
+++ test/movie_data_source_test.dart
@@ -14,7 +14,7 @@ void main() {
   setUp(() {
     dio = MockDio(); // MockDio 객체 생성
     movieDataSourceImpl =
-        MovieDataSourceImpl(dio: dio, apiKey: '1'); // MovieDataSourceImpl 객체 생성
+        MovieDataSourceImpl(dio: dio, apiKey: ''); // MovieDataSourceImpl 객체 생성
   });
 
   test('mock_movie_data_source_test', () async {
@@ -24,18 +24,27 @@ void main() {
             queryParameters: any(named: 'queryParameters')))
         .thenAnswer((_) async => Response(
               requestOptions: RequestOptions(
-                path: '1',
+                path: '',
                 headers: {},
               ),
               data: {
                 'results': [
                   {
-                    'id': 0,
-                    'title': 'title',
-                    'overview': 'overview',
-                    'poster_path': 'poster_path',
-                    'vote_average': 0.0,
-                    'release_date': 'release_date',
+                    "adult": false,
+                    "backdrop_path": "/cJvUJEEQ86LSjl4gFLkYpdCJC96.jpg",
+                    "genre_ids": [10752, 28],
+                    "id": 1241436,
+                    "original_language": "en",
+                    "original_title": "Warfare",
+                    "overview":
+                        "\"실화 기반, 전장의 숨결을 생생히 담아낸 리얼 타임 전쟁 드라마\"  2006년 이라크 라마디. 미 해군 특수부대 네이비 씰 팀이 적진 한복판에서 감시 임무를 수행하던 중, 예상치 못한 상황에 휘말리게 된다. 민가를 점거해 잠복하던 그들은 알카에다 세력의 포위망에 갇히고, 지원은 끊긴 채 생존을 위한 사투를 벌이게 된다. 전직 네이비 씰이자 실제 생존자인 레이 멘도자 감독과 '엑스 마키나', '시빌 워'의 알렉스 가란드 감독이 공동 연출한 이 작품은, 전투의 혼돈과 공포를 실시간으로 그려낸다. 화려한 음악이나 영웅 서사 없이, 전장의 소음과 침묵, 그리고 병사들의 숨결이 화면을 가득 채운다. 실화를 바탕으로 한 이 영화는, 전쟁의 참혹함과 인간의 생존 본능을 적나라하게 보여주며, 관객을 전장의 한가운데로 끌어들인다. 과연 이들은 살아남을 수 있을까...",
+                    "popularity": 574.5249,
+                    "poster_path": "/j8tqBXwH2PxBPzbtO19BTF9Ukbf.jpg",
+                    "release_date": "2025-04-09",
+                    "title": "워페어",
+                    "video": false,
+                    "vote_average": 7.125,
+                    "vote_count": 228
                   }
                 ]
               },
@@ -44,6 +53,6 @@ void main() {
 
     final result = await movieDataSourceImpl.fetchNowPlayingMovies();
 
-    expect(result[0].title, 'title');
+    expect(result[0].title, '워페어');
   });
 }
